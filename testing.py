f32fe98d4a90264f8541b792c1b7939d4909b620
rykola8 <150235321+rykola8@users.noreply.github.com>
2024-05-15 21:08:34 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 import json
 
-joke_file = open('jokes.json') # opening JSON file
+joke_file = open('jokes.json', encoding='utf-8') # opening JSON file
 jokes = json.load(joke_file) # returns JSON object as a dictionary
 joke_file.close() # Closing file
 
-print(jokes[0])
+for joke in jokes:
+    print(joke["joke"])
